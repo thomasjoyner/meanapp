271bfcabd9ca102c59421af18948d93cf0c35e79
thomasjoyner <joyner.thomas@gmail.com>
2015-06-01 23:16:00 -0400

    CRUD for movies

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,9 +10,13 @@
  */
 angular
   .module('clientApp', [
-    'ngRoute'
+    'ngRoute',
+    'restangular'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, RestangularProvider) {
+    
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -22,7 +26,40 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl'
+      })
+      .when('/create/movie', {
+        templateUrl: 'views/movie-add.html',
+        controller: 'MovieAddCtrl'
+      })
+      .when('/movie/:id', {
+        templateUrl: 'views/movie-view.html',
+        controller: 'MovieViewCtrl'
+      })
+      .when('/movie/:id/delete', {
+        templateUrl: 'views/movie-delete.html',
+        controller: 'MovieDeleteCtrl'
+      })
+      .when('/movie/:id/edit', {
+        templateUrl: 'views/movie-edit.html',
+        controller: 'MovieEditCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  })
+
+  //This is strictly to remove the underscore in _id so that restangular can handle it
+  .factory('MovieRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+
+  .factory('Movie', function(MovieRestangular) {
+    return MovieRestangular.service('movie');
   });
